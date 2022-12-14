c75875c033ddbc45d6d0f5614ad0b738d4c1e795
supersujin <100469679+supersujin@users.noreply.github.com>
2022-12-14 23:43:11 +0900

    Update WaterBasic.cs
    Error code edit

--- Assets/Standard Assets/Environment/Water (Basic)/Scripts/WaterBasic.cs
+++ Assets/Standard Assets/Environment/Water (Basic)/Scripts/WaterBasic.cs
@@ -19,14 +19,14 @@ namespace UnityStandardAssets.Water
                 return;
             }
 
-            Vector4 waveSpeed = mat.GetVector("WaveSpeed");
-            float waveScale = mat.GetFloat("_WaveScale");
+//            Vector4 waveSpeed = mat.GetVector("WaveSpeed");
+            //float waveScale = mat.GetFloat("_WaveScale");
             float t = Time.time / 20.0f;
 
-            Vector4 offset4 = waveSpeed * (t * waveScale);
-            Vector4 offsetClamped = new Vector4(Mathf.Repeat(offset4.x, 1.0f), Mathf.Repeat(offset4.y, 1.0f),
-                Mathf.Repeat(offset4.z, 1.0f), Mathf.Repeat(offset4.w, 1.0f));
-            mat.SetVector("_WaveOffset", offsetClamped);
+            //Vector4 offset4 = waveSpeed * (t * waveScale);
+            //Vector4 offsetClamped = new Vector4(Mathf.Repeat(offset4.x, 1.0f), Mathf.Repeat(offset4.y, 1.0f),
+            //    Mathf.Repeat(offset4.z, 1.0f), Mathf.Repeat(offset4.w, 1.0f));
+            //mat.SetVector("_WaveOffset", offsetClamped);
         }
     }
 }
